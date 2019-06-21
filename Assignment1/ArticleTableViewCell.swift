a02ced86d909480edbc2df8f07463cc6b23fe5d2
Pasuparthi Shubha Tarun <pasuparthi.tarun@Tarun-PP-C02YQAUYLVCG.local>
2019-06-21 17:29:05 +0530

    Main storyboard and first screen

---
+++ Assignment1/ArticleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleTableViewCell.swift
+//  Assignment1
+//
+//  Created by Pasuparthi Shubha Tarun on 21/06/19.
+//  Copyright © 2019 Pasuparthi Shubha Tarun. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+//    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var title: UILabel!
+//    @IBOutlet weak var subTitle: UILabel!
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+}
---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Pasuparthi Shubha Tarun on 21/06/19.
+//  Copyright © 2019 Pasuparthi Shubha Tarun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var Table: UITableView!
+    @IBOutlet weak var SearchBar: UISearchBar!
+}
+
